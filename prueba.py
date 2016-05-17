3e8ce03f061e10d2b997a922ea1229baae640585
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-05-17 16:41:50 -0500

    Sd

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ import re, sys, signal, os, time, datetime
 import requests
 from smartcard.System import readers
 from smartcard.util import toHexString
-##import opc
+import opc
 
 # se definen los APDUs 
 # 
@@ -10,7 +10,7 @@ COMMAND = [0xff, 0xCA, 0x00, 0x00, 0x00]
 
 ####### FACECANDY ####### 
 numLEDs = 512
-#cliente = opc.Client('localhost:7890')
+cliente = opc.Client('localhost:7890')
 negro = [ (0,0,0) ] * numLEDs
 blanco = [ (255,255,255) ] * numLEDs
 
@@ -18,9 +18,9 @@ blanco = [ (255,255,255) ] * numLEDs
 
 def encender_led():
     cliente.put_pixels(negro)
-    time.sleep(.5)
+    time.sleep(.1)
     cliente.put_pixels(blanco)
-    time.sleep(2)
+    time.sleep(1.5)
     cliente.put_pixels(negro)
 
 
@@ -38,30 +38,37 @@ if __name__ == '__main__':
     print 'lectores disponibles', r
     lector = r[0]
     print 'Usando :', lector
-    conexion= lector.createConnection()
-    conexion.connect()
-    #### enviar datos ####
-    data, sw1, sw2 = conexion.transmit(COMMAND)
 
-    while True:
-        print "Command: %02X %02X" % (sw1, sw2)
-        a = sw1
-        print a
-        if a == 144:
-            print 'datos >:', data
-            v = data[::-1]
-            print v
-            hexa = toHexString(v)
-            hexan = hexa.split( )
-            ##print hexa, hexan
-            tam = len(hexan)
-            print 'leng', tam
-            cadena = ''
+    while(1):
+        try:
+            conexion= lector.createConnection()
+            conexion.connect()
+            #### enviar datos ####
+            data, sw1, sw2 = conexion.transmit(COMMAND)
 
-            for x in range(tam):
-                cadena = cadena + str(hexan[x])
+            #print "Command: %02X %02X" % (sw1, sw2)
+            a = sw1
+            #print a
+            if a == 144:
+                #print 'datos >:', data
+                v = data[::-1]
+                #print v
+                hexa = toHexString(v)
+                hexan = hexa.split( )
+                ##print hexa, hexan
+                tam = len(hexan)
+                #print 'leng', tam
+                cadena = ''
 
-            print cadena
-            decimal = int(cadena,16)
-            print decimal
+                for x in range(tam):
+                    cadena = cadena + str(hexan[x])
 
+                print cadena
+                decimal = int(cadena,16)
+                print decimal
+                encender_led()
+                sumar_puntos(str(decimal),'10')
+              # Read data from RFID reader
+        # 
+        except Exception, e:
+            continue
